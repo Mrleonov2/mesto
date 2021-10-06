5b270e4c01d4df8434b706b21640145db4f0b13e
Mrleonov2 <mikhail-leonov2002@yandex.ru>
2021-10-06 23:01:46 +0700

    К 1 ревью(1)

--- script.js
+++ script.js
@@ -1,36 +1,43 @@
 let popup = document.querySelector(".popup"),
-  popupOpenBtn = document.querySelector(".profile__editButton"),
-  popupCloseBtn = popup.querySelector(".popup__close");
+  popupOpenBtn = document.querySelector(".profile__button-edit"),
+  popupCloseBtn = popup.querySelector(".popup__button-close");
 // Находим форму в DOM
-let formElement = document.querySelector(".form");
+let formElement = document.querySelector(".popup__container");
 // Находим поля формы в DOM
-let nameInput = popup.querySelector("popup__user_name"),
-  jobInput = popup.querySelector("popup__user_job");
-// Получите значение полей jobInput и nameInput из свойства value
-
+let inputName = popup.querySelector(".popup__input_type_name"),
+  inputJob = popup.querySelector(".popup__input_type_job");
 // Выберите элементы, куда должны быть вставлены значения полей
-let profileName = document.querySelector("profile__name"),
-  profileJob = document.querySelector("profile__job");
+let profileName = document.querySelector(".profile__name"),
+  profileJob = document.querySelector(".profile__job");
+// Получите значение полей jobInput и nameInput из свойства value
+function getInputValue() {
+  (inputName.value = profileName.textContent),
+    (inputJob.value = profileJob.textContent);
+}
 // Вставьте новые значения с помощью textContent
 
 function popupToggle() {
   popup.classList.toggle("popup_opened");
-
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
 }
 function clickOverlay(event) {
   if (event.target === event.currentTarget) {
     popupToggle();
   }
 }
-popup.addEventListener("click", clickOverlay);
 popupOpenBtn.addEventListener("click", popupToggle);
+popupOpenBtn.addEventListener("click", getInputValue);
+popup.addEventListener("click", clickOverlay);
 popupCloseBtn.addEventListener("click", popupToggle);
+
+let likeBtn = document.querySelectorAll(".cards__button-like");
+function likeActive() {
+  likeBtn.classList.toggle("cards__button-like_active");
+}
+
+likeBtn.addEventListener("click",likeActive);
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-} // Эта строчка отменяет стандартную отправку формы.
+}
+// Эта строчка отменяет стандартную отправку формы.
 formElement.addEventListener("submit", formSubmitHandler);
-
-(nameInput.value = profileName), (jobInput.value = profileJob);
